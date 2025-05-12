0a20fc3d068a9dbddb243fb399ea7bcd6dc1e0ed
kervingc <kervin.garcia@mxglobalsolutions.com>
2025-05-12 15:07:42 -0700

    First app

---
+++ FirstApp/App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.dummyText}>Another piece of text!</Text>
+      </View>
+      <Text
+        style={{ margin: 16, borderWidth: 2, borderColor: "red", padding: 16 }}
+      >
+        Hello World!
+      </Text>
+      <Button title="Tap me!" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dummyText: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor: "red",
+    padding: 16,
+  },
+});

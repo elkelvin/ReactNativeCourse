f5806a8381e425b6fca8bed66912cf6054b96031
kervingc <kegarciaca@ittepic.edu.mx>
2025-09-10 16:00:45 -0700

    Fix: The category card component is implemented within FlatList.

--- AppMeal/screens/CategoriesScreen.js
+++ AppMeal/screens/CategoriesScreen.js
@@ -1,15 +1,18 @@
 import { View, FlatList, Text } from "react-native";
 import { CATEGORIES } from '../data/data'
+import CategoryGridTitle from "../components/CategoryGridTitle";
+
+const renderItem = (itemData) => {
+    return <CategoryGridTitle title={itemData.item.title} color={itemData.item.color} />
+}
 const CategoriesScreen = () => {
     return (
-        <View>
-            {/* {CATEGORIES.map(c => <Text key={c.id}>{c.title}</Text>)} */}
-            <FlatList
-                keyExtractor={(item, index) => item.id}
-                data={CATEGORIES}
-                renderItem={({ item }) => (<Text>{item.title}</Text>)
-                }
-            />
-        </View>);
+        < FlatList
+            keyExtractor={(item, index) => item.id}
+            data={CATEGORIES}
+            renderItem={renderItem}
+            numColumns={2}
+        />
+    );
 }
 export default CategoriesScreen;

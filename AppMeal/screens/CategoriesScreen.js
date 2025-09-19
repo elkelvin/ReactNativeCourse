bb807c171c3cc70676c6df1e42b950cbdcfea610
kervingc <kegarciaca@ittepic.edu.mx>
2025-09-18 20:07:19 -0700

    Feat: Navigation is added to switch from categories to MealsOverview

--- AppMeal/screens/CategoriesScreen.js
+++ AppMeal/screens/CategoriesScreen.js
@@ -1,18 +1,30 @@
 import { View, FlatList, Text } from "react-native";
-import { CATEGORIES } from '../data/data'
+import { CATEGORIES } from "../data/data";
 import CategoryGridTitle from "../components/CategoryGridTitle";
+import { useNavigation } from "@react-navigation/native";
 
-const renderItem = (itemData) => {
-    return <CategoryGridTitle title={itemData.item.title} color={itemData.item.color} />
-}
-const CategoriesScreen = () => {
+const CategoriesScreen = ({ navigation }) => {
+  const renderItem = (itemData) => {
+    const onPressHandler = () => {
+      navigation.navigate("MealsOverview");
+    };
+
     return (
-        < FlatList
-            keyExtractor={(item, index) => item.id}
-            data={CATEGORIES}
-            renderItem={renderItem}
-            numColumns={2}
-        />
+      <CategoryGridTitle
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={onPressHandler}
+      />
     );
-}
+  };
+
+  return (
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderItem}
+      numColumns={2}
+    />
+  );
+};
 export default CategoriesScreen;

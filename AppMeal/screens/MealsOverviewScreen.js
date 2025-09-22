abf9424d6b1b3cd51f12e0dc493b5fda4e049891
elkelvin <kegarciaca@ittepic.edu.mx>
2025-09-22 13:32:52 -0700

    Fix: Text import included.

--- AppMeal/screens/MealsOverviewScreen.js
+++ AppMeal/screens/MealsOverviewScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import { MEALS } from "../data/data";
 const MealsOverviewScreen = () => {
   return (

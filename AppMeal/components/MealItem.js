a83f67e3dc265b248b43f82c59b95eef1c6da9dc
elkelvin <kegarciaca@ittepic.edu.mx>
2025-09-24 11:24:45 -0700

    Feat: The Meal Item component is created

---
+++ AppMeal/components/MealItem.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+const MealItem = ({title} ) => {
+    return (
+        <View>
+            <Text>{item.title}</Text>
+        </View>
+    );
+}
+export default MealItem;

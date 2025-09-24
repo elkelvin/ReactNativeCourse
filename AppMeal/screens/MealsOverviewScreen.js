810335c95a557cfffa0095efa1805aecdc9d2cc3
elkelvin <kegarciaca@ittepic.edu.mx>
2025-09-24 13:22:02 -0700

    Feat: Rendering meals using the meal item component

--- AppMeal/screens/MealsOverviewScreen.js
+++ AppMeal/screens/MealsOverviewScreen.js
@@ -1,9 +1,22 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, FlatList } from "react-native";
 import { MEALS } from "../data/data";
-const MealsOverviewScreen = () => {
+import MealItem from "../components/MealItem";
+const MealsOverviewScreen = ({ route }) => {
+  const categoryId = route.params.categoryId;
+  const displayMeals = MEALS.filter(mealItem => mealItem.categoryIds.includes(categoryId));
+
+  const renderMealItem = (item) => {
+    return <MealItem item={item.item.title} />
+  }
+
   return (
     <View style={style.container}>
-      <Text>Meals Overview Screen</Text>
+      <Text>Meals Overview Screen {categoryId}</Text>
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={displayMeals}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };

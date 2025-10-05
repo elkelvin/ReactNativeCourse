67075831685ed4a7ebabc2c0ea56d88578843009
elkelvin <kegarciaca@ittepic.edu.mx>
2025-10-05 13:10:53 -0700

    Feature: Implement functionality to add and remove favorite meals
    Add logic to allow the user to add and remove meals from the favorites list, improving interaction with the app

--- AppMeal/screens/MealDetailScreen.js
+++ AppMeal/screens/MealDetailScreen.js
@@ -3,24 +3,33 @@ import { MEALS } from "../data/data";
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
-import { useLayoutEffect } from "react";
+import { useContext, useLayoutEffect } from "react";
 import IconButton from "../components/IconButton";
+import { FavoritesContext } from "../store/context/favorites-context";
 const MealDetailScreen = ({ route, navigation }) => {
+    const favoriteMealsCtx = useContext(FavoritesContext);
+
     const mealId = route.params.mealId;
     const meal = MEALS.find(x => x.id === mealId);
 
-
-    const onHeaderButtonPressHandler = () => {
-        console.log("Hello!");
+    const mealIsFavorite = favoriteMealsCtx.ids.includes(mealId);
+    const onChangeStatusHandler = () => {
+        if (mealIsFavorite) {
+            favoriteMealsCtx.removeFavorite(mealId);
+        } else {
+            favoriteMealsCtx.addFavorite(mealId);
+        }
     }
 
     useLayoutEffect(() => {
         navigation.setOptions({
             headerRight: () => {
-                return <IconButton icon="star" color="white" onPress={onHeaderButtonPressHandler} />
+                return <IconButton icon={
+                    mealIsFavorite ? "star" : "star-outline"
+                } color="white" onPress={onChangeStatusHandler} />
             }
         });
-    }, [navigation]);
+    }, [navigation, onChangeStatusHandler]);
 
     return (<ScrollView style={styles.rootContainer}>
         <Image source={{ uri: meal.imageUrl }} style={styles.image} />

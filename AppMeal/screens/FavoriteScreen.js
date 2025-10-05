15b980a3aaae8b748e9b980da05c629d9bcd8ae4
elkelvin <kegarciaca@ittepic.edu.mx>
2025-10-05 13:14:06 -0700

    Feature: Add list of favorite meals and not found message
    Implement functionality to display a list of favorite meals and a custom message when no meals are found in that list.

--- AppMeal/screens/FavoriteScreen.js
+++ AppMeal/screens/FavoriteScreen.js
@@ -1,7 +1,33 @@
-import { View, Text } from "react-native";
+import { useContext } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { FavoritesContext } from "../store/context/favorites-context";
+import MealsList from "../components/MealsList/MealsList";
+import { MEALS } from "../data/data";
 const FavoriteScreen = () => {
-    return (<View>
-        <Text>JEJEJ</Text>
-    </View>)
+    const favoriteMealCtx = useContext(FavoritesContext);
+    const favoriteMeals = MEALS.filter(meal => favoriteMealCtx.ids.includes(meal.id));
+
+    if (favoriteMeals.length === 0) {
+        return <View style={styles.rootContainer}>
+            <Text style={text}>You have no favorite meals yet</Text>
+        </View>
+    }
+
+    return (
+        <MealsList items={favoriteMeals} />
+    )
 }
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: "white"
+    }
+});
 export default FavoriteScreen;

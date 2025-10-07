203461941427d0e094c2e39a8f9226efb8c45655
elkelvin <kegarciaca@ittepic.edu.mx>
2025-10-07 09:23:34 -0700

    Feat: Implement Redux with useSelector for retrieving favorite meals
    Set up Redux store and use useSelector to fetch favorite meals from the state

--- AppMeal/screens/FavoriteScreen.js
+++ AppMeal/screens/FavoriteScreen.js
@@ -1,12 +1,14 @@
-import { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { FavoritesContext } from "../store/context/favorites-context";
+// import { useContext } from "react";
+// import { FavoritesContext } from "../store/context/favorites-context";
+import { useSelector } from 'react-redux';
 import MealsList from "../components/MealsList/MealsList";
 import { MEALS } from "../data/data";
 const FavoriteScreen = () => {
-    const favoriteMealCtx = useContext(FavoritesContext);
-    const favoriteMeals = MEALS.filter(meal => favoriteMealCtx.ids.includes(meal.id));
-
+    // const favoriteMealCtx = useContext(FavoritesContext);
+    // const favoriteMeals = MEALS.filter(meal => favoriteMealCtx.ids.includes(meal.id));
+    const favoriteMealsIds = useSelector((state) => state.favoriteMeals.ids);
+    const favoriteMeals = MEALS.filter(meal => favoriteMealsIds.includes(meal.id));
     if (favoriteMeals.length === 0) {
         return <View style={styles.rootContainer}>
             <Text style={text}>You have no favorite meals yet</Text>

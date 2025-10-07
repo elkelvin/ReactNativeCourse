d6c6f6d3bdf5079009c16bd687a362a7b5c68248
elkelvin <kegarciaca@ittepic.edu.mx>
2025-10-07 13:07:04 -0700

    Feat: Add navigation to the main App component

--- Tracker/App.js
+++ Tracker/App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { BottomTabs } from 'react-native-screens';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
+import ManageExpense from './screen/ManageExpense';
+import RecentExpenses from './screen/RecentExpenses';
+import AllExpenses from './screen/AllExpenses';
+import { GlobalStyle } from './constans/globalStyle';
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+const BottomNavigator = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BottomTabs.Navigator screenOptions={{
+      headerStyle: { backgroundColor: GlobalStyle.colors.primary500 },
+      headerTintColor: "white",
+      tabBarStyle: { backgroundColor: GlobalStyle.colors.primary500 },
+      tabBarActiveTintColor: GlobalStyle.colors.accent500
+    }}>
+      <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} options={{
+        title: "Recent Expenses",
+        tabBarLabel: "Recent",
+        tabBarIcon: ({ color, size }) => (<Ionicons name="hourglass" size={size} color={color} />)
+      }} />
+      <BottomTabs.Screen name="AllExpenses" component={AllExpenses} options={{
+        title: "All Expenses",
+        tabBarLabel: "All Expenses",
+        tabBarIcon: ({ color, size }) => (<Ionicons name="calendar" size={size} color={color} />)
+      }} />
+      
+    </BottomTabs.Navigator>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ExpensesOverview" component={BottomNavigator} options={{ headerShown: false }} />
+          <Stack.Screen name="ManageExpense" component={ManageExpense} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}

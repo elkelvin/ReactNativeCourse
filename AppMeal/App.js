2f59c9908f64145ed33301405eade4a683882f2b
elkelvin <kegarciaca@ittepic.edu.mx>
2025-10-07 09:23:56 -0700

    Feat: Wrap application with Redux Provider to integrate the store
    Integrate Redux store by wrapping the application with the Redux Provider component.

--- AppMeal/App.js
+++ AppMeal/App.js
@@ -3,12 +3,15 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Ionicons } from "@expo/vector-icons";
+import { Provider } from 'react-redux';
+
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
 import FavoriteScreen from "./screens/FavoriteScreen";
-import { Ionicons } from "@expo/vector-icons";
-import FavoritesContextProvider from "./store/context/favorites-context";
+// import FavoritesContextProvider from "./store/context/favorites-context";
+import { store } from './store/redux/store';
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
@@ -37,7 +40,8 @@ export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <FavoritesContextProvider>
+      {/* <FavoritesContextProvider> */}
+      <Provider store={store}>
         <NavigationContainer>
           <Stack.Navigator screenOptions={{
             headerStyle: { backgroundColor: '#351401' },
@@ -50,7 +54,8 @@ export default function App() {
             <Stack.Screen name="MealDetail" component={MealDetailScreen} options={{ title: "About the Meal" }} />
           </Stack.Navigator>
         </NavigationContainer>
-      </FavoritesContextProvider>
+      </Provider>
+      {/* </FavoritesContextProvider> */}
     </>
   );
 }

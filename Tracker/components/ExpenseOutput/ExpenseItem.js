21716c4af7a563add307237109c3f4cd21258d3c
elkelvin <kegarciaca@ittepic.edu.mx>
2025-10-10 15:21:19 -0700

    Feat: Add basic list item structure for expense entries

---
+++ Tracker/components/ExpenseOutput/ExpenseItem.js
@@ -0,0 +1,56 @@
+import { Pressable, View, StyleSheet, Text } from "react-native";
+import { GlobalStyle } from "../../constans/globalStyle";
+import { getFormattedDate } from "../../utils/date";
+
+const ExpenseItem = ({ description, amount, date }) => {
+    return (
+        <Pressable>
+            <View style={styles.expenseItem}>
+                <View>
+                    <Text style={[styles.textBase, styles.description]}>{description}</Text>
+                    <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
+                </View>
+                <View style={styles.amountContainer}>
+                    <Text style={styles.amount}>{amount.toFixed(2)}</Text>
+                </View>
+            </View>
+        </Pressable>);
+}
+
+const styles = StyleSheet.create({
+    expenseItem: {
+        padding: 12,
+        marginVertical: 8,
+        backgroundColor: GlobalStyle.colors.primary500,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        borderRadius: 6,
+        elevation: 3,
+        shadowColor: GlobalStyle.colors.gray500,
+        shadowRadius: 4,
+        shadowOffset: { width: 1, height: 1 },
+        shadowOpacity: 0.4
+    },
+    textBase: {
+        color: GlobalStyle.colors.primary50
+    },
+    description: {
+        fontSize: 16,
+        marginBottom: 4,
+        fontWeight: "bold"
+    },
+    amountContainer: {
+        paddingHorizontal: 12,
+        paddingVertical: 4,
+        backgroundColor: "white",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 4,
+        minWidth: 80
+    },
+    amount: {
+        color: GlobalStyle.colors.primary500,
+        fontWeight: "bold"
+    }
+});
+export default ExpenseItem;

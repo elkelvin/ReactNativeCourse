dcc81478375c41ae7b0535e333497d437401d022
elkelvin <kegarciaca@ittepic.edu.mx>
2025-10-10 15:22:03 -0700

    Feat: Develop component for showing global expense data overview

---
+++ Tracker/components/ExpenseOutput/ExpensesSummary.js
@@ -0,0 +1,31 @@
+import { View, Text, StyleSheet } from "react-native";
+import { GlobalStyle } from "../../constans/globalStyle";
+
+const ExpensesSummary = ({ expenses, periodName }) => {
+    const expensesSum = expenses.reduce((sum, expense) => sum + expense.amount, 0);
+    return (<View style={styles.container}>
+        <Text style={styles.period}>{periodName}</Text>
+        <Text style={styles.sum}>${expensesSum.toFixed(2)}</Text>
+    </View>);
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 8,
+        backgroundColor: GlobalStyle.colors.primary50,
+        borderRadius: 6,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    period: {
+        fontSize: 12,
+        color: GlobalStyle.colors.primary400
+    },
+    sum: {
+        fontSize: 16,
+        fontWeight: "bold",
+        color: GlobalStyle.colors.primary500
+    }
+});
+export default ExpensesSummary;

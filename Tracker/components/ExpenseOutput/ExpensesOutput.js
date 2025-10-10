35534bf5a57b8528e1cb51a90c2fae99bd70582b
elkelvin <kegarciaca@ittepic.edu.mx>
2025-10-10 15:21:53 -0700

    Feat: Create reusable component for rendering expenses and totals

---
+++ Tracker/components/ExpenseOutput/ExpensesOutput.js
@@ -0,0 +1,52 @@
+import { View, StyleSheet } from "react-native";
+import ExpensesSummary from "./ExpensesSummary";
+import ExpensesList from "./ExpensesList";
+import { GlobalStyle } from "../../constans/globalStyle";
+const DUMMY_EXPENSES = [
+    {
+        id: "e1",
+        description: "A pair of shoes",
+        amount: 59.99,
+        date: new Date("2021-12-19")
+    },
+    {
+        id: "e2",
+        description: "A pair of trousers",
+        amount: 89.99,
+        date: new Date("2022-01-05")
+    },
+    {
+        id: "e3",
+        description: "Some bananas",
+        amount: 5.99,
+        date: new Date("2021-12-01")
+    },
+    {
+        id: "e4",
+        description: "A book",
+        amount: 14.99,
+        date: new Date("2022-02-19")
+    },
+    {
+        id: "e5",
+        description: "A book",
+        amount: 18.99,
+        date: new Date("2022-02-18")
+    }
+];
+const ExpensesOutput = ({ expenses, expensesPeriod }) => {
+    return (<View style={styles.container}>
+        <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
+        <ExpensesList data={DUMMY_EXPENSES} />
+    </View>);
+}
+
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 24,
+        paddingTop: 24,
+        paddingBottom: 0,
+        backgroundColor: GlobalStyle.colors.primary700,
+    },
+});
+export default ExpensesOutput;

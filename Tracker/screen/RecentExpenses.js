cedad040f2ce8da64de1722b17bb8e22bfb97c52
elkelvin <kegarciaca@ittepic.edu.mx>
2025-10-14 19:37:56 -0700

    Feature: Add return of expenses within the last seven days using context

--- Tracker/screen/RecentExpenses.js
+++ Tracker/screen/RecentExpenses.js
@@ -1,6 +1,16 @@
-import { View } from "react-native";
+import { useContext } from "react";
 import ExpensesOutput from "../components/ExpenseOutput/ExpensesOutput";
+import { ExpensesContext } from "../store/ExpensesContext";
+import { getDateMinusDate } from "../utils/date";
 const RecentExpenses = () => {
-    return (<ExpensesOutput expensesPeriod="Last 7 days"/>)
+    const expensesCtx = useContext(ExpensesContext);
+
+    const recentExpenses = expensesCtx.expenses.filter((expense) => {
+        const today = new Date();
+        const date7DaysAgo = getDateMinusDate(today, 7);
+        return expense.date > date7DaysAgo;
+    });
+
+    return (<ExpensesOutput expensesPeriod="Last 7 days" expenses={recentExpenses} fallbackText="No expenses registered for the last 7 days" />)
 }
 export default RecentExpenses;

3d1223573bf9f9f321a065c247631c56b1ca02b6
elkelvin <kegarciaca@ittepic.edu.mx>
2025-10-14 19:38:11 -0700

    Feature: Add functionality to return all expenses

--- Tracker/screen/AllExpenses.js
+++ Tracker/screen/AllExpenses.js
@@ -1,5 +1,8 @@
+import { useContext } from "react";
 import ExpensesOutput from "../components/ExpenseOutput/ExpensesOutput";
+import { ExpensesContext } from "../store/ExpensesContext";
 const AllExpenses = () => {
-    return (<ExpensesOutput expensesPeriod="Total" />);
+    const expenseCtx = useContext(ExpensesContext);
+    return (<ExpensesOutput expensesPeriod="Total" expenses={expenseCtx.expenses} fallbackText="No registered expenses found"/>);
 }
 export default AllExpenses;

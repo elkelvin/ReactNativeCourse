61276b4d2080ff1283dad6a061bdabc3b12f0985
elkelvin <kegarciaca@ittepic.edu.mx>
2025-11-04 09:58:33 -0700

    Feat: Fetch expenses in the recent expenses window

--- Tracker/screen/RecentExpenses.js
+++ Tracker/screen/RecentExpenses.js
@@ -1,9 +1,37 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import ExpensesOutput from "../components/ExpenseOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/ExpensesContext";
 import { getDateMinusDate } from "../utils/date";
+import { getExpenses } from "../utils/http";
+import LoadingOverlay from '../UI/LoadingOverlay';
+import ErrorOverlay from "../UI/ErrorOverlay";
+
 const RecentExpenses = () => {
+    const [isFetching, setIsFetching] = useState(true);
+    const [error, setError] = useState();
     const expensesCtx = useContext(ExpensesContext);
+
+    useEffect(() => {
+        const getExpense = async () => {
+            setIsFetching(true);
+            try {
+                const expenses = await getExpenses();
+            }
+            catch (error) {
+                setError("Could not fecth expenses");
+            }
+            setIsFetching(false);
+            expensesCtx.setExpenses(expenses);
+        };
+        getExpense();
+    }, []);
+
+    if (isFetching) {
+        return <LoadingOverlay />
+    }
+    if (error && !isFetching) {
+        return <ErrorOverlay error={error} onConfirm={() => setError()} />
+    }
 
     const recentExpenses = expensesCtx.expenses.filter((expense) => {
         const today = new Date();

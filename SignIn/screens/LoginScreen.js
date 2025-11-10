b69b17b69319f22e0b1817c60eecaf97da74b40b
elkelvin <kegarciaca@ittepic.edu.mx>
2025-11-10 11:09:38 -0700

    Feat: An authentication process has been added to the two windows for logging in and creating users.

--- SignIn/screens/LoginScreen.js
+++ SignIn/screens/LoginScreen.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
-
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { login } from '../util/auth';
 const LoginScreen = () => {
-    return <AuthContent isLogin />;
+    const [showSpinner, setShowSpinner] = useState(false);
+
+    const onLoginHandler = async ({ email, password }) => {
+        setShowSpinner(true);
+        await login(email, password);
+        setShowSpinner(false);
+    }
+
+    if (showSpinner) {
+        return <LoadingOverlay message="Login in" />;
+    }
+    return <AuthContent isLogin onAuthenticate={onLoginHandler} />;
 }
 
 export default LoginScreen;
--- SignIn/screens/SignupScreen.js
+++ SignIn/screens/SignupScreen.js
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+import { createUser } from '../util/auth';
 import AuthContent from '../components/Auth/AuthContent';
-
+import LoadingOverlay from '../components/ui/LoadingOverlay'
 const SignupScreen = () => {
-    return <AuthContent />;
+    const [showSpinner, setShowSpinner] = useState(false);
+    const onSignUpHandler = async ({ email, password }) => {
+        setShowSpinner(true);
+        await createUser(email, password);
+        setShowSpinner(false);
+    }
+
+    if (showSpinner) {
+        return <LoadingOverlay message="Creating User..."/>
+    }
+    return <AuthContent onAuthenticate={onSignUpHandler} />;
 }
 
 export default SignupScreen;

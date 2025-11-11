9f5fe8a19ee05cdb20d99dd987b1f11f1ef7fbd7
elkelvin <kegarciaca@ittepic.edu.mx>
2025-11-11 12:16:35 -0700

    Feat: feat: Implement API consumption example using a token.

--- SignIn/screens/WelcomeScreen.js
+++ SignIn/screens/WelcomeScreen.js
@@ -1,10 +1,23 @@
+import { useContext, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import axios from 'axios';
+import { AuthContext } from '../store/auth-context';
 const WelcomeScreen = () => {
+    const [fetchedMessage, setFetchedMessage] = useState("");
+    const context = useContext(AuthContext);
+    const token = context.token;
+    useEffect(() => {
+        axios.get("https://react-native-3ee7a-default-rtdb.firebaseio.com/message.json?auth=" + token)
+            .then((response) => {
+                setFetchedMessage(response.data);
+            });
+    }, [token]);
+
     return (
         <View style={styles.rootContainer}>
             <Text style={styles.title}>Welcome!</Text>
             <Text>You authenticated successfully!</Text>
+            <Text>{fetchedMessage}</Text>
         </View>
     );
 }

1f76bee1e252b2fcaf898beed8c6b8b5aa233d94
elkelvin <kegarciaca@ittepic.edu.mx>
2025-11-11 12:16:48 -0700

    Feat: Add function to register the token in the context.

--- SignIn/screens/LoginScreen.js
+++ SignIn/screens/LoginScreen.js
@@ -1,18 +1,26 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
 import { login } from '../util/auth';
+import { Alert } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 const LoginScreen = () => {
     const [showSpinner, setShowSpinner] = useState(false);
+    const context = useContext(AuthContext);
 
     const onLoginHandler = async ({ email, password }) => {
         setShowSpinner(true);
-        await login(email, password);
-        setShowSpinner(false);
+        try {
+            const token = await login(email, password);
+            context.authenticate(token);
+        } catch (Error) {
+            Alert.alert("Authentication failed!", "Could not log you in. Please check your credentials or try again lattter!");
+            setShowSpinner(false);
+        }
     }
 
     if (showSpinner) {
-        return <LoadingOverlay message="Login in" />;
+        return <LoadingOverlay message="Logging you in..." />;
     }
     return <AuthContent isLogin onAuthenticate={onLoginHandler} />;
 }
--- SignIn/screens/SignupScreen.js
+++ SignIn/screens/SignupScreen.js
@@ -1,17 +1,27 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { createUser } from '../util/auth';
 import AuthContent from '../components/Auth/AuthContent';
 import LoadingOverlay from '../components/ui/LoadingOverlay'
+import { Alert } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 const SignupScreen = () => {
     const [showSpinner, setShowSpinner] = useState(false);
+
+    const context = useContext(AuthContext);
+
     const onSignUpHandler = async ({ email, password }) => {
         setShowSpinner(true);
-        await createUser(email, password);
-        setShowSpinner(false);
+        try {
+            const token = await createUser(email, password);
+            context.authenticate(token);
+        } catch (error) {
+            Alert.alert("Authentication failed", "Could not create user, please check your input and try again later")
+            setShowSpinner(false);
+        }
     }
 
     if (showSpinner) {
-        return <LoadingOverlay message="Creating User..."/>
+        return <LoadingOverlay message="Creating User..." />
     }
     return <AuthContent onAuthenticate={onSignUpHandler} />;
 }
